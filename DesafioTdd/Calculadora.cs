e2802d8aef904a58b030fa9b7452bc325e2d0537
José Bento <joseluizbpj@gmail.com>
2025-05-02 18:59:11 -0300

    Primeiro commit

---
+++ DesafioTdd/Calculadora.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioTdd
+{
+    public class Calculadora
+    {
+        public int Somar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Subtrair(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public List<string> ApresentarHistorico()
+        {
+            return new List<string>();
+        }
+    }
+}
---
+++ DesafioTddTestes/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using DesafioTdd;
+
+namespace DesafioTddTestes;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData (1, 2, 3)]
+    [InlineData (4, 5, 9)]
+    public void TestarSomar(int num1, int num2, int resultado)
+    {
+        Calculadora calculadora = new Calculadora();
+
+        int resultadoCalculadora = calculadora.Somar(num1, num2);
+
+        Assert.Equal(resultado, resultadoCalculadora);
+    }
+
+    [Theory]
+    [InlineData (1, 2, 2)]
+    [InlineData (4, 5, 20)]
+    public void TestarMultiplicar(int num1, int num2, int resultado)
+    {
+        Calculadora calculadora = new Calculadora();
+
+        int resultadoCalculadora = calculadora.Multiplicar(num1, num2);
+
+        Assert.Equal(resultado, resultadoCalculadora);
+    }
+
+    [Theory]
+    [InlineData (1, 2, -1)]
+    [InlineData (4, 5, -1)]
+    public void TestarSubtrair(int num1, int num2, int resultado)
+    {
+        Calculadora calculadora = new Calculadora();
+
+        int resultadoCalculadora = calculadora.Subtrair(num1, num2);
+
+        Assert.Equal(resultado, resultadoCalculadora);
+    }
+    
+    [Theory]
+    [InlineData (4, 2, 2)]
+    [InlineData (6, 3, 2)]
+    public void TestarDividir(int num1, int num2, int resultado)
+    {
+        Calculadora calculadora = new Calculadora();
+
+        int resultadoCalculadora = calculadora.Dividir(num1, num2);
+
+        Assert.Equal(resultado, resultadoCalculadora);
+    }
+
+    [Fact]
+    public void TestarDividirPorZero()
+    {
+        Calculadora calc = new Calculadora();
+
+        Assert.Throws<DivideByZeroException>(() => calc.Dividir(1, 0));
+    }
+}
